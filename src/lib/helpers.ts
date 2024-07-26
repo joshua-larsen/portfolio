fc948a4732ae25581bcd506be4f72726934f5820
joshua-larsen <64284030+joshua-larsen@users.noreply.github.com>
2024-07-26 13:41:19 -0700

    update timezonecard.astro file

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -2,31 +2,31 @@ export function trimText(input: string, maxLength: number = 100): string {
   if (input.length <= maxLength) return input;
   return input.substring(0, maxLength - 3) + "...";
 }
-export function getCurrentTimeInItaly(): Date {
+export function getCurrentTimeInVancouver(): Date {
   // Create a date object with the current UTC time
   const now = new Date();
 
-  // Convert the UTC time to Italy's time
-  const offsetItaly = 2; // Italy is in Central European Summer Time (UTC+2), but you might need to adjust this based on Daylight Saving Time
-  now.setHours(now.getUTCHours() + offsetItaly);
+  // Convert the UTC time to Vancouver's time
+  const offsetVancouver = - 7; // Vancouver is in Pacific Standard Time (UTC-7), but you might need to adjust this based on Daylight Saving Time
+  now.setHours(now.getUTCHours() + offsetVancouver);
 
   return now;
 }
 
-export function formatTimeForItaly(date: Date): string {
+export function formatTimeForVancouver(date: Date): string {
   const options: Intl.DateTimeFormatOptions = {
     hour: "numeric",
     minute: "2-digit",
     second: "2-digit",
-    hour12: true, // This will format the time in 12-hour format with AM/PM
-    timeZone: "Europe/Rome",
+    hour12: false, // This will format the time in 12-hour format with AM/PM
+    timeZone: "America/Vancouver",
   };
 
   let formattedTime = new Intl.DateTimeFormat("en-US", options).format(date);
 
   // Append the time zone abbreviation. You can automate this with libraries like `moment-timezone`.
-  // For simplicity, here I'm just appending "CET", but do remember that Italy switches between CET and CEST.
-  formattedTime += " CET";
+  // For simplicity, here I'm just appending "PST", but do remember that Vancouver switches between PST and PDT.
+  formattedTime += " PST";
 
   return formattedTime;
 }
